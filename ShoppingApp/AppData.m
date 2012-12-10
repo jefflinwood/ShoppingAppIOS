fc934c78ed03bd754158bbf4fabbe81997d9c553
Jeffrey Linwood <jlinwood@gmail.com>
2012-12-10 13:28:56 -0600

    Add networking stubs for loading products and display products, also add AFNetworking to project

--- ShoppingApp/AppData.m
+++ ShoppingApp/AppData.m
@@ -8,9 +8,40 @@
 
 #import "AppData.h"
 
+#import "AFNetworking.h"
+
+@interface AppData ()
+//array of ProductDisplay objects
+@property (nonatomic, strong) NSArray *productDisplays;
+
+//key is nid (as NSString), value is Product object
+@property (nonatomic, strong) NSDictionary *products;
+
+- (void) loadDisplayProducts;
+- (void) loadProducts;
+
+@end
+
 @implementation AppData
 
+
 static AppData *sharedData = nil;
+
+#pragma mark Drupal Commerce Networking methods
+
+- (NSString*) baseURL {
+    return  @"http://commerce.localhost:8082/rest";
+}
+
+- (void) loadDisplayProducts {
+    
+}
+
+- (void) loadProducts {
+    
+}
+
+#pragma mark Singleton method
 
 //Anything we need to do to initialize the shared data singleton can go here.
 + (AppData*) sharedInstance
@@ -18,6 +49,8 @@ static AppData *sharedData = nil;
     static dispatch_once_t predicate;
     dispatch_once(&predicate, ^{
         sharedData = [[AppData alloc] init];
+        [sharedData loadDisplayProducts];
+        [sharedData loadProducts];
     });
     return sharedData;
 }
