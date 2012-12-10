4eedfcbc02426767acd4e28a322ab58cd1cd2d36
Jeffrey Linwood <jlinwood@gmail.com>
2012-12-10 08:57:14 -0600

    Initial Commit

---
+++ ShoppingApp/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  ShoppingApp
+//
+//  Created by Jeffrey Linwood on 12/10/12.
+//  Copyright (c) 2012 Biscotti Labs, LLC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ ShoppingApp/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  ShoppingApp
+//
+//  Created by Jeffrey Linwood on 12/10/12.
+//  Copyright (c) 2012 Biscotti Labs, LLC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
