acccaebdf54f8a550ec52d235de7ef4f68e0b30b
Jeffrey Linwood <jlinwood@gmail.com>
2012-12-10 18:50:17 -0600

    Product Details View Controller and Storyboard for iPhone

---
+++ ShoppingApp/ProductDetailsViewController.h
@@ -0,0 +1,13 @@
+//
+//  ProductDetailsViewController.h
+//  ShoppingApp
+//
+//  Created by Jeffrey Linwood on 12/10/12.
+//  Copyright (c) 2012 Biscotti Labs, LLC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ProductDetailsViewController : UIViewController
+
+@end
---
+++ ShoppingApp/ProductDetailsViewController.m
@@ -0,0 +1,30 @@
+//
+//  ProductDetailsViewController.m
+//  ShoppingApp
+//
+//  Created by Jeffrey Linwood on 12/10/12.
+//  Copyright (c) 2012 Biscotti Labs, LLC. All rights reserved.
+//
+
+#import "ProductDetailsViewController.h"
+
+@interface ProductDetailsViewController ()
+
+@end
+
+@implementation ProductDetailsViewController
+
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
